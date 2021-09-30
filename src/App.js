0c9f2e89b7fd935fb40bbabdea5e23a65b8f0c3f
PasserbyHua <867471083@qq.com>
2021-09-30 15:13:51 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import React, { Component } from 'react'
- 
+
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello,World!</h1>
+                <h1>RPGGame</h1>
+                <form action="">
+                    <label>
+                        <input className="inputText" type="text" placeholder="账号" />
+                    </label>
+                    <br />
+                    <label>
+                        <input className="inputText" type="password" placeholder="密码" />
+                    </label>
+                </form>
+                <input id="loginBtn" className="loginBtn" type="button" value="登录" />
+                <p>没有账号? <a href="http://www.baidu.com">点此注册</a></p>
             </div>
         )
     }
